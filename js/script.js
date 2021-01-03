2ad361bc33a3b231029238005b52b0d82228dbae
Adam Bishow <39249320+abish5@users.noreply.github.com>
2021-01-02 19:20:31 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,17 +3,18 @@ const subTabs = document.getElementsByClassName("tab");
 
 for (const tab of subTabs) {
     tab.addEventListener('click', function() {
+        for(var i = 0; i < 5; i++)
+        {
+            subTabs[i].style.color = '#408ade';
+            subTabs[i].style.backgroundColor = 'white';
+        }
+
         tab.style.color = 'white';
         tab.style.backgroundColor = '#408ade';
+
         if(tab == subTabs[0])
         {
-            for(var i = 0; i < 5; i++)
-            {
-                if(i!=0) {
-                    subTabs[i].style.color = '#408ade';
-                    subTabs[i].style.backgroundColor = '#ghostwhite';
-                }
-            }
+
             document.getElementById("vision-tab").hidden = false;
             document.getElementById("mission-tab").hidden = true;
             document.getElementById("objective-tab").hidden = true;
@@ -22,13 +23,7 @@ for (const tab of subTabs) {
         }
         if(tab == subTabs[1])
         {
-            for(var i = 0; i < 5; i++)
-            {
-                if(i!=1) {
-                    subTabs[i].style.color = '#408ade';
-                    subTabs[i].style.backgroundColor = 'ghostwhite';
-                }
-            }
+
             document.getElementById("vision-tab").hidden = true;
             document.getElementById("mission-tab").hidden = false;
             document.getElementById("objective-tab").hidden = true;
@@ -37,13 +32,7 @@ for (const tab of subTabs) {
         }
         if(tab == subTabs[2])
         {
-            for(var i = 0; i < 5; i++)
-            {
-                if(i!=2) {
-                    subTabs[i].style.color = '#408ade';
-                    subTabs[i].style.backgroundColor = 'ghostwhite';
-                }
-            }
+
             document.getElementById("vision-tab").hidden = true;
             document.getElementById("mission-tab").hidden = true;
             document.getElementById("objective-tab").hidden = false;
@@ -52,13 +41,6 @@ for (const tab of subTabs) {
         }
         if(tab == subTabs[3])
         {
-            for(var i = 0; i < 5; i++)
-            {
-                if(i!=3) {
-                    subTabs[i].style.color = '#408ade';
-                    subTabs[i].style.backgroundColor = 'ghostwhite';
-                }
-            }
             document.getElementById("vision-tab").hidden = true;
             document.getElementById("mission-tab").hidden = true;
             document.getElementById("objective-tab").hidden = true;
@@ -67,14 +49,6 @@ for (const tab of subTabs) {
         }
         if(tab == subTabs[4])
         {
-            for(var i = 0; i < 5; i++)
-            {
-                if(i!=4) {
-                    subTabs[i].style.color = '#408ade';
-                    subTabs[i].style.backgroundColor = 'ghostwhite';
-                }
-                
-            }
             document.getElementById("vision-tab").hidden = true;
             document.getElementById("mission-tab").hidden = true;
             document.getElementById("objective-tab").hidden = true;
